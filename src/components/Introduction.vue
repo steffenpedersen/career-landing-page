64751660ffdf96cc26364da62b70cea4faf8500b
steffenpedersen <steffen.pedersen@live.dk>
2020-04-30 13:35:11 +0200

    Add styling to introduction

---
+++ src/components/Introduction.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="c-introduction">
+    <header class="c-header">
+      <img class="c-header__logo" src="../assets/svg/grafikr_logo.svg" alt />
+    </header>
+    <div class="c-introduction__wrap">
+      <img class="c-introduction__circle" src="../assets/svg/grafikr_circle.svg" alt />
+      <h1 class="c-introduction__title">Prisvindende e-commerce bureau</h1>
+    </div>
+    <img class="c-introduction__image-meeting" src="../assets/images/grafikr_meeting.jpg" alt />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Introduction"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../css/components/header.scss";
+@import "../css/components/introduction.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+    <Introduction/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Introduction from '@/components/Introduction.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Introduction
   }
 }
 </script>
