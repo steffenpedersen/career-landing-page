d8d5375568df589141976f9397a6ff8a434599d9
steffenpedersen <steffen.pedersen@live.dk>
2020-05-01 12:08:07 +0200

    Add offset to colleagues

--- src/components/Colleagues.vue
+++ src/components/Colleagues.vue
@@ -2,23 +2,83 @@
   <section class="c-colleagues">
     <h3 class="c-colleagues__heading">Mød dine (måske) nye kollegaer</h3>
     <div class="c-colleagues__all">
-        <span class="c-colleague c-colleague__kiranan" data-aos="fade-in" data-aos-delay="100">Kiranan</span>
-        <span class="c-colleague c-colleague__erik" data-aos="fade-in" data-aos-delay="200">Erik</span>
-        <span class="c-colleague c-colleague__anders" data-aos="fade-in" data-aos-delay="300">Anders</span>
-        <span class="c-colleague c-colleague__lasse" data-aos="fade-in" data-aos-delay="400">Lasse</span>
-        <span class="c-colleague c-colleague__simone" data-aos="fade-in" data-aos-delay="500">Simone</span>
-      </div>
-    <div class="c-colleagues__all">
-        <span class="c-colleague c-colleague__magnus" data-aos="fade-in" data-aos-delay="600">Magnus</span>
-        <span class="c-colleague c-colleague__rasmus_p" data-aos="fade-in" data-aos-delay="700">Rasmus P</span>
-        <span class="c-colleague c-colleague__christian" data-aos="fade-in" data-aos-delay="800">Christian</span>
-        <span class="c-colleague c-colleague__stefan" data-aos="fade-in" data-aos-delay="900">Stefan</span>
+      <span
+        class="c-colleague c-colleague__kiranan"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="100"
+      >Kiranan</span>
+      <span
+        class="c-colleague c-colleague__erik"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="200"
+      >Erik</span>
+      <span
+        class="c-colleague c-colleague__anders"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="300"
+      >Anders</span>
+      <span
+        class="c-colleague c-colleague__lasse"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="400"
+      >Lasse</span>
+      <span
+        class="c-colleague c-colleague__simone"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="500"
+      >Simone</span>
     </div>
     <div class="c-colleagues__all">
-        <span class="c-colleague c-colleague__rasmus_m" data-aos="fade-in" data-aos-delay="1000">Rasmus M</span>
-        <span class="c-colleague c-colleague__tor" data-aos="fade-in" data-aos-delay="1100">Tor</span>
-        <span class="c-colleague c-colleague__thomas" data-aos="fade-in" data-aos-delay="1200">Thomas</span>
-      </div>
+      <span
+        class="c-colleague c-colleague__magnus"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="600"
+      >Magnus</span>
+      <span
+        class="c-colleague c-colleague__rasmus_p"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="700"
+      >Rasmus P</span>
+      <span
+        class="c-colleague c-colleague__christian"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="800"
+      >Christian</span>
+      <span
+        class="c-colleague c-colleague__stefan"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="900"
+      >Stefan</span>
+    </div>
+    <div class="c-colleagues__all">
+      <span
+        class="c-colleague c-colleague__rasmus_m"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="1000"
+      >Rasmus M</span>
+      <span
+        class="c-colleague c-colleague__tor"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="1100"
+      >Tor</span>
+      <span
+        class="c-colleague c-colleague__thomas"
+        data-aos="fade-in"
+        data-aos-offset="-200"
+        data-aos-delay="1200"
+      >Thomas</span>
+    </div>
   </section>
 </template>
 
